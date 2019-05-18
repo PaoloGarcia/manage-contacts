4b8c7fdaff0ad19029bea96e0f3ebd4b7f23400d
PaoloGarcia <paolo.hernandezg@outlook.com>
2019-05-18 18:14:11 -0500

    install react router

--- src/components/Contacts/AddContact/AddContact.js
+++ src/components/Contacts/AddContact/AddContact.js
@@ -2,27 +2,49 @@ import React from "react";
 import "./add-contact.css";
 
 const AddContact = () => {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [phone, setPhone] = React.useState("");
+
     return (
         <div className="add-contact">
-            <div className="add-contact-header">
+            {/* <div className="add-contact-header">
                 Add Contact
-            </div>
+            </div> */}
             <div className="add-contact-body">
                 <form onSubmit={e => e.preventDefault()}>
                     <div className="add-contact-body-item">
-                        <input type="text" placeholder="Full Name" />
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Full Name"
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                        />
                     </div>
                     <div className="add-contact-body-item">
-                        <input type="email" placeholder="Email" />
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
                     </div>
                     <div className="add-contact-body-item">
-                        <input type="text" placeholder="Phone Number" />
+                        <input
+                            type="text"
+                            name="phone"
+                            placeholder="Phone Number"
+                            value={phone}
+                            onChange={e => setPhone(e.target.value)}
+                        />
                     </div>
                     <button
                         className="btn-add"
                         type="submit"
                     >
-                        Add
+                        Add Contact
                     </button>
                 </form>
             </div>
