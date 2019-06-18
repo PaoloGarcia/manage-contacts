d85ee9d89966c5b5b95806543cc6fd18feedaad8
PaoloGarcia <paolo.hernandezg@outlook.com>
2019-06-17 20:15:58 -0500

    create index.js of server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+// middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
