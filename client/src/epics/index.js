3d1bfcffe3565209e9d672546e13fd99ef8e2c33
PaoloGarcia <paolo.hernandezg@outlook.com>
2019-08-29 00:25:59 -0500

    remove import actions in the epic file

--- client/src/epics/index.js
+++ client/src/epics/index.js
@@ -5,17 +5,12 @@ import {
     GET_CONTACTS, GET_CONTACTS_SUCCESS, GET_CONTACTS_FAILURE,
 } from "../actions/types";
 
-import {
-    getContactsSuccess,
-    getContactsFailure,
-} from "../actions";
-
-function getContactsEpic(action$, state$) {
+function getContactsEpic(action$) {
     return action$
         .pipe(
             // filter(action => action.type === GET_CONTACTS),
             ofType(GET_CONTACTS),
-            mergeMap(async action => {
+            mergeMap(async () => {
                 try {
                     const res = await fetch("http://localhost:5000/contacts");
                     const contacts = await res.json();
